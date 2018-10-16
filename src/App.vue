2c7dc5280fe600ca0b5706a2502af35892f52ec9
adilimudassir <mudassiradili4u@gmail.com>
2018-10-16 21:44:45 +0100

    fetch access token when app is instantiated

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/users">Users</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "app",
+  methods: {
+    ...mapActions(["fetchAccessToken"])
+  },
+  created() {
+    this.fetchAccessToken();
+  }
+};
+</script>
+
 
 <style lang="scss">
 #app {
